d3200e75793ab0263aec8b1fc8f752ce0f5b1ae5
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-03-20 22:42:50 +0300

    добавил описания схем и всякое.

--- spice_tools.py
+++ spice_tools.py
@@ -1,13 +1,33 @@
-import os
-import subprocess
-
 from langchain_core.tools import tool
+from spicelib import SimRunner, SpiceEditor, AscEditor
+from spicelib.simulators.ltspice_simulator import LTspice
 
 
 @tool
-def spice_tool(circuit_description: str) -> str:
-    """Run circuit simulation using ngspice."""
-    netlist_file: str = "generated_circuit.cir"
+def spice_tool(asc: str) -> str:
+    """Run circuit simulation with LTspice using .asc file contents."""
+
+    asc_file: str = "temp/circuit.asc"
+
+    with open(asc_file, 'w') as file:
+        file.write(asc)
+
+    runner = SimRunner(simulator=LTspice, output_folder='temp')
+    netlist = AscEditor(asc_file)
+
+    runner.run(netlist, exe_log=True)
+
+    raws = []
+
+    for raw, log in runner:
+        #print(raw)
+        print("Raw file: %s, Log file: %s" % (raw, log))
+        raws.append(raw)
+
+
+    return open(raws[0], "r").read()
+
+    """
 
     with open(netlist_file, 'w') as file:
         file.write(circuit_description)
@@ -27,4 +47,4 @@ def spice_tool(circuit_description: str) -> str:
         os.remove(raw_file)  # Удаляем временный файл после чтения
         return table_data
     else:
-        return "Simulation completed, but output data was not found."
+        return "Simulation completed, but output data was not found."""
